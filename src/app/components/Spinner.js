5a67dd7d5f5c0da7e92b3f82bf9e8cc483e52b4c
Zowhy <dtrovato892@gmail.com>
2024-06-19 18:22:56 -0300

    Loading 19/06

---
+++ src/app/components/Spinner.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./spinner.module.css";
+
+export default function Spinner() {
+    return (
+        <div className={styles.spinnerContainer}>
+            <Image width={100} height={100} alt="Loading" src={"/6-dots-rotate.svg"} className={styles.image} />
+        </div>
+    );
+}
